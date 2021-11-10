ca2f0941a67d2c835415f57f9aa2cd6585cfc3b2
CARDILE Simone <Simone.CARDILE@zerouno.loc>
2021-11-10 21:40:31 +0100

    init settings

--- js/gol.js
+++ js/gol.js
@@ -1,10 +1,75 @@
-const rows = 40;
-const cols = 40;
-let currGen = [];
-let nextGen = [];
-let started = false;
-let timer;
-let evolutionSpeed = 500;
+var rows;
+var cols;
+var currGen;
+var nextGen;
+var started = false;
+var timer;
+var evolutionSpeed = 500;
+var settingsModal = new bootstrap.Modal(document.getElementById('settingsModal'), {
+	backdrop: 'static',
+	keyboard: false
+});
+
+function loadSetting() {
+	rows = document.getElementById('rows').value;
+	cols = document.getElementById('cols').value;
+	currGen = [rows];
+	nextGen = [rows];
+	evolutionSpeed = document.getElementById('evolutionSpeed').value;
+}
+
+function cellClick() {
+	let loc = this.id.split("_");
+	let row = Number(loc[0]);
+	let col = Number(loc[1]);
+	if (this.className === 'alive') {
+		this.setAttribute('class', 'dead');
+		currGen[row][col] = 0;
+	} else {
+		this.setAttribute('class', 'alive');
+		currGen[row][col] = 1;
+	}
+}
+
+function createWorld() {
+	let world = document.querySelector('#world');
+	world.innerHTML = '';
+	world.classList.add('table-responsive');
+	world.classList.add('mt-3');
+	let tbl = document.createElement('table');
+	tbl.setAttribute('id', 'worldGridTable');
+	tbl.setAttribute('class', 'table');
+	let tbo = document.createElement('tbody');
+	for (let i = 0; i < rows; i++) {
+		let tr = document.createElement('tr');
+		for (let j = 0; j < cols; j++) {
+			let cell = document.createElement('td');
+			cell.setAttribute('id', i + '_' + j);
+			cell.setAttribute('class', 'dead');
+			cell.addEventListener('click', cellClick);
+			tr.appendChild(cell);
+		}
+		tbo.appendChild(tr);
+	}
+	tbl.appendChild(tbo);
+	world.appendChild(tbl);
+}
+
+function createGenArrays() {
+	for (let i = 0; i < rows; i++) {
+		currGen[i] = new Array(cols);
+		nextGen[i] = new Array(cols);
+	}
+}
+
+function initGenArrays() {
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			currGen[i][j] = 0;
+			nextGen[i][j] = 0;
+		}
+	}
+}
 
 function getNeighborCount(row, col) {
 	let count = 0;
@@ -107,7 +172,7 @@ function evolve() {
 }
 
 function startStopGol() {
-	let startstop = document.querySelector('#buttonStartStop');
+	let startstop = document.querySelector('#startStopButton');
 	if (!started) {
 		started = true;
 		startstop.value = 'Stop Reproducing';
@@ -127,61 +192,22 @@ function resetWorld() {
 	updateWorld();
 }
 
-function createWorld() {
-	let world = document.querySelector('#world');
-	world.classList.add('table-responsive');
-	world.classList.add('mt-3');
-	let tbl = document.createElement('table');
-	tbl.setAttribute('id', 'worldGridTable');
-	tbl.setAttribute('class', 'table');
-	let tbo = document.createElement('tbody');
-	for (let i = 0; i < rows; i++) {
-		let tr = document.createElement('tr');
-		for (let j = 0; j < cols; j++) {
-			let cell = document.createElement('td');
-			cell.setAttribute('id', i + '_' + j);
-			cell.setAttribute('class', 'dead');
-			cell.addEventListener('click', cellClick);
-			tr.appendChild(cell);
-		}
-		tbo.appendChild(tr);
-	}
-	tbl.appendChild(tbo);
-	world.appendChild(tbl);
-}
-
-function createGenArrays() {
-	for (let i = 0; i < rows; i++) {
-		currGen[i] = new Array(cols);
-		nextGen[i] = new Array(cols);
-	}
-}
-
-function initGenArrays() {
-	for (let i = 0; i < rows; i++) {
-		for (let j = 0; j < cols; j++) {
-			currGen[i][j] = 0;
-			nextGen[i][j] = 0;
-		}
-	}
-}
-
-function cellClick() {
-	let loc = this.id.split("_");
-	let row = Number(loc[0]);
-	let col = Number(loc[1]);
-	if (this.className === 'alive') {
-		this.setAttribute('class', 'dead');
-		currGen[row][col] = 0;
-	} else {
-		this.setAttribute('class', 'alive');
-		currGen[row][col] = 1;
-	}
-}
-window.onload = () => {
+function reloadWorld() {
+	loadSetting();
 	createWorld();
 	createGenArrays();
-	initGenArrays();
-	document.querySelector('#buttonStartStop').addEventListener('click', startStopGol);
-	document.querySelector('#buttonReset').addEventListener('click', resetWorld);
+	resetWorld();
+}
+
+document.querySelector('#startStopButton').addEventListener('click', startStopGol);
+document.querySelector('#resetButton').addEventListener('click', resetWorld);
+document.querySelector('#settingsButton').addEventListener('click', function() {
+	settingsModal.show();
+});
+document.querySelector('#changeSettingsButton').addEventListener('click', function() {
+	reloadWorld();
+	settingsModal.hide();
+});
+window.onload = () => {
+	reloadWorld();
 }
