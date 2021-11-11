d2edb5382fa8eeced44a79a9be04329b0dacbe32
CARDILE Simone <Simone.CARDILE@zerouno.loc>
2021-11-11 13:04:12 +0100

    beautifierment

--- js/gol.js
+++ js/gol.js
@@ -1,15 +1,21 @@
+// Righe e colonne della tabella (mondo)
 var rows;
 var cols;
+// 2D arrays
 var currGen;
 var nextGen;
+// Viene settata a true quando viene avviata l'evoluzione
 var started = false;
+// Util per il loop di evoluzione
 var timer;
-var evolutionSpeed = 500;
+// Durata del ciclo di evoluzione
+var evolutionSpeed;
 var settingsModal = new bootstrap.Modal(document.getElementById('settingsModal'), {
 	backdrop: 'static',
 	keyboard: false
 });
 
+// Settaggio delle variabili principali utilizzando i valori della form di configurazione
 function loadSetting() {
 	rows = document.getElementById('rows').value;
 	cols = document.getElementById('cols').value;
@@ -22,6 +28,7 @@ function cellClick() {
 	let loc = this.id.split("_");
 	let row = Number(loc[0]);
 	let col = Number(loc[1]);
+	// Toggle viva o morta
 	if (this.className === 'alive') {
 		this.setAttribute('class', 'dead');
 		currGen[row][col] = 0;
@@ -31,6 +38,7 @@ function cellClick() {
 	}
 }
 
+// Creazione mondo (tabella)
 function createWorld() {
 	let world = document.querySelector('#world');
 	world.innerHTML = '';
@@ -55,6 +63,7 @@ function createWorld() {
 	world.appendChild(tbl);
 }
 
+// Creazione generazioni (corrente e nuova)
 function createGenArrays() {
 	for (let i = 0; i < rows; i++) {
 		currGen[i] = new Array(cols);
@@ -62,6 +71,7 @@ function createGenArrays() {
 	}
 }
 
+// Creazione array dimensionali
 function initGenArrays() {
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < cols; j++) {
@@ -75,46 +85,69 @@ function getNeighborCount(row, col) {
 	let count = 0;
 	let nrow = Number(row);
 	let ncol = Number(col);
+
+	// SE NO prima riga
 	if (nrow - 1 >= 0) {
+		// Check cella sopra
 		if (currGen[nrow - 1][ncol] == 1) {
 			count++;
 		}
-	}
-	if (nrow - 1 >= 0 && ncol - 1 >= 0) {
-		if (currGen[nrow - 1][ncol - 1] == 1) {
-			count++;
+		// SE NO prima colonna
+		if (ncol - 1 >= 0) {
+			// Check cella in alto sinistra
+			if (currGen[nrow - 1][ncol - 1] == 1) {
+				count++;
+			}
+		}
+
+		// SE NO ultima colonna
+		if (ncol + 1 < cols) {
+			// Check cella in alto a destra
+			if (currGen[nrow - 1][ncol + 1] == 1) {
+				count++;
+			}
 		}
 	}
-	if (nrow - 1 >= 0 && ncol + 1 < cols) {
-		if (currGen[nrow - 1][ncol + 1] == 1) {
+
+	// SE NO ultima riga
+	if (nrow + 1 < rows) {
+		// Check cella sotto
+		if (currGen[nrow + 1][ncol] == 1) {
 			count++;
 		}
+		// SE NO prima colonna
+		if (ncol - 1 >= 0) {
+			// Check cella in basso a sinistra
+			if (currGen[nrow + 1][ncol - 1] == 1) {
+				count++;
+			}
+		}
+
+		// SE NO ultima colonna
+		if (ncol + 1 < cols) {
+			// Check cella in basso a destra
+			if (currGen[nrow + 1][ncol + 1] == 1) {
+				count++;
+			}
+		}
 	}
+
+	// SE NO prima colonna
 	if (ncol - 1 >= 0) {
+		// Check cella a sinistra
 		if (currGen[nrow][ncol - 1] == 1) {
 			count++;
 		}
 	}
+
+	// SE NO ultima colonna
 	if (ncol + 1 < cols) {
+		// Check cella a destra
 		if (currGen[nrow][ncol + 1] == 1) {
 			count++;
 		}
 	}
-	if (nrow + 1 < rows && ncol - 1 >= 0) {
-		if (currGen[nrow + 1][ncol - 1] == 1) {
-			count++;
-		}
-	}
-	if (nrow + 1 < rows && ncol + 1 < cols) {
-		if (currGen[nrow + 1][ncol + 1] == 1) {
-			count++;
-		}
-	}
-	if (nrow + 1 < rows) {
-		if (currGen[nrow + 1][ncol] == 1) {
-			count++;
-		}
-	}
+
 	return count;
 }
 
@@ -122,7 +155,9 @@ function createNextGen() {
 	for (row in currGen) {
 		for (col in currGen[row]) {
 			let neighbors = getNeighborCount(row, col);
+			// Regole
 			if (currGen[row][col] == 1) {
+				// SE la cella è viva
 				if (neighbors < 2) {
 					nextGen[row][col] = 0;
 				} else if (neighbors == 2 || neighbors == 3) {
@@ -131,6 +166,7 @@ function createNextGen() {
 					nextGen[row][col] = 0;
 				}
 			} else if (currGen[row][col] == 0) {
+				// SE la cella è morta
 				if (neighbors == 3) {
 					nextGen[row][col] = 1;
 				}
@@ -142,12 +178,15 @@ function createNextGen() {
 function updateCurrGen() {
 	for (row in currGen) {
 		for (col in currGen[row]) {
+			// Aggiornamento la generazione corrente con i dati della nuova
 			currGen[row][col] = nextGen[row][col];
+			// Reset della nuova generazione
 			nextGen[row][col] = 0;
 		}
 	}
 }
 
+// Aggiornamento del mondo (tabella) sulla base della generazione corrente
 function updateWorld() {
 	let cell = '';
 	for (row in currGen) {
@@ -163,14 +202,19 @@ function updateWorld() {
 }
 
 function evolve() {
+	// Applicazione delle regole
 	createNextGen();
+	// Aggiornamento della generazione corrente sulla base della nuova
 	updateCurrGen();
+	// Aggiornamento del mondo (tabella)
 	updateWorld();
 	if (started) {
+		// Set del loop di evoluzione
 		timer = setTimeout(evolve, evolutionSpeed);
 	}
 }
 
+// Start e Stop dell'evoluzione
 function startStopGol() {
 	let startstop = document.querySelector('#startStopButton');
 	if (!started) {
@@ -185,20 +229,29 @@ function startStopGol() {
 }
 
 function resetWorld() {
+	// SE l'evoluzione è in corso
 	if (started) {
+		// STOP
 		startStopGol();
 	}
+	// Reset generazioni (corrente e nuova)
+	createGenArrays();
+	// Creazione array dimensionali
 	initGenArrays();
+	// Aggiornamento del mondo (tabella) sulla base della generazione corrente
 	updateWorld();
 }
 
 function reloadWorld() {
+	// Caricamento variabili di evoluzione
 	loadSetting();
+	// Creazione del mondo (tabella)
 	createWorld();
-	createGenArrays();
+	// Reset delle generazioni ed eventuale stop dell'evoluzione in corso
 	resetWorld();
 }
 
+// Gestione degli eventi
 document.querySelector('#startStopButton').addEventListener('click', startStopGol);
 document.querySelector('#resetButton').addEventListener('click', resetWorld);
 document.querySelector('#settingsButton').addEventListener('click', function() {
